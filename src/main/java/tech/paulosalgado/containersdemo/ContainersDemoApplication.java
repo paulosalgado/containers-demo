d714741d36845b72e19f6db70e306910f6cca957
Paulo Salgado <pjosalgado@gmail.com>
2019-10-20 00:09:30 -0300

    project initialized

---
+++ src/main/java/tech/paulosalgado/containersdemo/ContainersDemoApplication.java
@@ -0,0 +1,13 @@
+package tech.paulosalgado.containersdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainersDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainersDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/paulosalgado/containersdemo/ContainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.paulosalgado.containersdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
