30024597cc0a0afe399c7a9f25e2f9b13ffe5156
Paulo Salgado <pjosalgado@gmail.com>
2019-10-20 01:32:27 -0300

    building maven project

---
+++ maven-demo/src/main/java/tech/paulosalgado/mavendemo/User.java
@@ -0,0 +1,55 @@
+package tech.paulosalgado.mavendemo;
+
+import java.util.Objects;
+
+public class User {
+
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
